14e4f71b6862445c0b22445b956de5219011a94a
SarahKirsten <deranged.link@gmail.com>
2016-09-22 13:19:53 -0700

    Initial commit of EX03_05

---
+++ EX03_05/EX03_05/Header.h
@@ -0,0 +1,19 @@
+/////////////////////////
+//Program Name: EX03_05
+//Name: Sarah Kirsten
+//Class and Section: CS172-1
+//Date: 9/22/2016
+///////////////////////
+
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+
+
+#pragma once
---
+++ EX03_05/EX03_05/Source.cpp
@@ -0,0 +1,17 @@
+/////////////////////////
+//Program Name: EX03_05
+//Name: Sarah Kirsten
+//Class and Section: CS172-1
+//Date: 9/22/2016
+///////////////////////
+
+/*Liang Programming Exercise 10.10: the MyInteger class*/
+#include "Header.h"
+
+int main()
+{
+
+
+
+	return 0;
+}
