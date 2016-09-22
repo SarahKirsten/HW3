ea0603d893a2a7c9834a42d638dc64d720c07645
SarahKirsten <deranged.link@gmail.com>
2016-09-22 13:10:52 -0700

    Initial commit of EX03_02

---
+++ EX03_02/EX03_02/Header.h
@@ -0,0 +1,17 @@
+/////////////////////////
+//Program Name: EX03_02
+//Name: Sarah Kirsten
+//Class and Section: CS172-1
+//Date: 9/22/2016
+///////////////////////
+
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+
---
+++ EX03_02/EX03_02/Source.cpp
@@ -0,0 +1,17 @@
+/////////////////////////
+//Program Name: EX03_02
+//Name: Sarah Kirsten
+//Class and Section: CS172-1
+//Date: 9/22/2016
+///////////////////////
+
+/*Liang Programming Exercise 9.6: Algebra: quadratic equations*/
+#include "Header.h"
+
+int main()
+{
+
+
+
+	return 0;
+}
