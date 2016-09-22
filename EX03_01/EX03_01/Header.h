d3d08c449e272d35752343051e551984d2f207e2
SarahKirsten <deranged.link@gmail.com>
2016-09-22 13:05:16 -0700

    Initial commit for EX03_01 basic header and source files

---
+++ EX03_01/EX03_01/Header.h
@@ -0,0 +1,16 @@
+/////////////////////////
+//Program Name: EX03_01
+//Name: Sarah Kirsten
+//Class and Section: CS172-1
+//Date: 9/22/2016
+///////////////////////
+
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
---
+++ EX03_01/EX03_01/Source.cpp
@@ -0,0 +1,16 @@
+/////////////////////////
+//Program Name: EX03_01
+//Name: Sarah Kirsten
+//Class and Section: CS172-1
+//Date: 9/22/2016
+///////////////////////
+
+#include "Header.h"
+
+int main()
+{
+
+
+
+	return 0;
+}
