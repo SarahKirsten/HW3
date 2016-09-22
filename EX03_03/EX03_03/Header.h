45f283c9e0cecf0a908266352ac4006485feecf6
SarahKirsten <deranged.link@gmail.com>
2016-09-22 13:13:55 -0700

    Initial commit of EX03_03

---
+++ EX03_03/EX03_03/Header.h
@@ -0,0 +1,19 @@
+/////////////////////////
+//Program Name: 
+//Name: Sarah Kirsten
+//Class and Section: CS172-1
+//Date: 
+///////////////////////
+
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+
+
+#pragma once
---
+++ EX03_03/EX03_03/Source.cpp
@@ -0,0 +1,17 @@
+/////////////////////////
+//Program Name: 
+//Name: Sarah Kirsten
+//Class and Section: CS172-1
+//Date: 
+///////////////////////
+
+/*Liang Programming Exercise 9.11: the EvenNumber class*/
+#include "Header.h"
+
+int main()
+{
+
+
+
+	return 0;
+}
