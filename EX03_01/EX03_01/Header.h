90086f442b1e7e8a0f958d9d24471e4e09070269
SarahKirsten <deranged.link@gmail.com>
2016-09-25 14:46:47 -0700

    EX03_01 FINISHED

--- EX03_01/EX03_01/Header.h
+++ EX03_01/EX03_01/Header.h
@@ -14,4 +14,61 @@
 #include <fstream>
 using namespace std;
 
+/*create a class named Fan
+int data field named speed that specifies the speed of the fan. has three speeds indicated with a value 1, 2, or 3
+
+bool data field named on that specifies whether the fan is on
+
+double data field named radius that specifies the radius of the fan
+
+a no-arg constructor that creates a default fan with speed 1, on false, and radius 5.
+
+the accessor and mutator functions for all the data fields
+*/
+
+class Fan
+{
+public://data field
+	int speed;//three speeds 1, 2, or 3
+	bool on;//is the fan on or not
+	double radius;//what's the radius of the fan
+
+	Fan()//no-arg constructor
+	{
+		speed = 1;//default speed
+		on = false;//default on or off
+		radius = 5;//default fan radius
+	}
+
+	int getSpeed()//accessor
+	{
+		return speed;
+	}
+
+	void setSpeed(int setSpeed)//mutator
+	{
+		speed = setSpeed;
+	}
+
+	bool getOn()//accessor
+	{
+		return on;
+	}
+
+	void setOn(bool trueOrFalse)//mutator
+	{
+		on = trueOrFalse;
+	}
+
+	double getRadius()//accessor
+	{
+		return radius;
+	}
+
+	void setRadius(double setRadius)//mutator
+	{
+		radius = setRadius;
+	}
+};
+
 #pragma once
--- EX03_01/EX03_01/Source.cpp
+++ EX03_01/EX03_01/Source.cpp
@@ -6,12 +6,46 @@
 ///////////////////////
 
 #include "Header.h"
-/*Liang Programming Exercise 9.2: the Fan Class*/
+/*pg: 367 Exercise 9.2: the Fan Class*/
+
+//The class exists in the Header file.
+
+/*draw the UML diagram for the class. implement the class. write a test program that creates two Fan objects. 
+assign speed 3, radius 10, and turn it on to the first object.
+assign speed 2, radius 5, and turn it off to the second object. invoke their accessor functions to display the fan properties*/
 
 int main()
 {
+	string answer;
+	cout << "Properties of first fan: " << endl;
+	Fan fan1;
+	fan1.setSpeed(3);
+	fan1.setOn(true);
+	fan1.setRadius(10);
 
+	cout << "Fan speed: " << fan1.getSpeed() << endl;
+	cout << "Fan radius: " << fan1.getRadius() << endl;
 
+	if (fan1.getOn() == true)
+		answer = "yes";
+	else
+		answer = "no";
+	cout << "Fan on? " << answer << endl << endl;
+	
+	Fan fan2;
+	fan2.setSpeed(2);
+	fan2.setRadius(5);
+	fan2.setOn(false);
+
+	cout << "Properties of first fan: " << endl;
+	cout << "Fan speed: " << fan2.getSpeed() << endl;
+	cout << "Fan radius: " << fan2.getRadius() << endl;
+
+	if (fan2.getOn() == true)
+		answer = "yes";
+	else
+		answer = "no";
+	cout << "Fan on? " << answer << endl << endl;
 
 	return 0;
 }
