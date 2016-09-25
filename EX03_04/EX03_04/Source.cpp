dffedd1823aadd3b49bb61f21be3ab8864100129
SarahKirsten <deranged.link@gmail.com>
2016-09-25 16:51:47 -0700

    EX03_04 FINISHED

--- EX03_04/EX03_04/Source.cpp
+++ EX03_04/EX03_04/Source.cpp
@@ -8,9 +8,42 @@
 /*Liang Programming Exercise 10.4: Sort characters in a string*/
 #include "Header.h"
 
+string sort(string &s)
+{
+	for (int i = s.length() - 1; i >= 1; i--)
+	{
+		//Find the max in the list [0...i]
+		char currentMax = s[0];
+		int currentMaxIndex = 0;
+
+		for (int j = 1; j <= i; j++)
+		{
+			if (currentMax < s[j])
+			{
+				currentMax = s[j];
+				currentMaxIndex = j;
+			}
+		}
+
+		//Swap list[i] with list[currentMaxIndex] if necessary;
+		if (currentMaxIndex != i)
+		{
+			s[currentMaxIndex] = s[i];
+			s[i] = currentMax;
+		}
+	}
+
+	return s;
+}
+
 int main()
 {
+	//Prompt user to enter a string
+	cout << "Enter a string s: ";
+	string s;
+	getline(cin, s);
 
+	cout << "The sorted string is " << sort(s) << endl << endl;
 
 
 	return 0;
